e77ddf69a02b230e7d067d0710f6346a013f2e2b
chankruze <chankruze@gmail.com>
2019-09-21 04:03:48 +0530

    Initial Commit For Official Release
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ omni_RMX1901.mk
@@ -0,0 +1,25 @@
+# Release name
+PRODUCT_RELEASE_NAME := RMX1901
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# For PBRP
+# $(call inherit-product, vendor/pb/config/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := RMX1901
+PRODUCT_NAME := omni_RMX1901
+PRODUCT_BRAND := Realme
+PRODUCT_MODEL := RMX1901
+PRODUCT_MANUFACTURER := Realme
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.treble.enabled=true \
+    ro.secure=1 \
+    ro.adb.secure=0 \
+    ro.vendor.build.security_patch=2099-12-31
