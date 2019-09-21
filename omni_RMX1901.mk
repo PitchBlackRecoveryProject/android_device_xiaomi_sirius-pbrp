979bff2b2b36eec309755eb492ba29bae374ce36
chankruze <chankruze@gmail.com>
2019-09-21 06:04:48 +0530

    Adapted for Pitch Black Recovery Project
    Signed-off-by: chankruze <chankruze@gmail.com>

--- omni_RMX1901.mk
+++ omni_RMX1901.mk
@@ -5,11 +5,8 @@ PRODUCT_RELEASE_NAME := RMX1901
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
-
 # For PBRP
-# $(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RMX1901
